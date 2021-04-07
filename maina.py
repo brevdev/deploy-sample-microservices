663e927db2d8cf67b775fffda4a64d97768f5f31
Drew Malin <drewmalin@gmail.com>
2021-04-07 15:17:38 -0700

    query

--- maina.py
+++ maina.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import brev
 
 def handler(request):
-    brev.db_query("foo", "SHOW DATABASES")
+    brev.db_query("foo", "SHOW TABLES;")
+    brev.db_query("foo", "SELECT COUNT(1);")
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     return "main.a - " + current_time
