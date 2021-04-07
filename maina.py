d08050019ca539d0917512cb06c9a6eae34d944c
Drew Malin <drewmalin@gmail.com>
2021-04-07 15:15:31 -0700

    fix typo

--- maina.py
+++ maina.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import brev
 
 def handler(request):
-    brev.db_query("SHOW DATABASES")
+    brev.db_query("foo", "SHOW DATABASES")
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     return "main.a - " + current_time
