1c1d3bb9e6970f927710e6587bc08582d3aa4f9d
Drew Malin <drewmalin@gmail.com>
2021-04-10 20:53:47 -0700

    crud

--- maina.py
+++ maina.py
@@ -1,8 +1,43 @@
 from datetime import datetime
 
 import brev
+import json
+
+foo_db = db("foo")
 
 def handler(request):
+    init_db()
+
+    path = request["requestContext"]["http"]["path"]
+    method = request["requestContext"]["http"]["method"]
+
+    if path == "/users":
+        if method == "GET":
+            data = get_users()
+            results = []
+            for k, v in data:
+                results.append(v)
+            return {
+                "statusCode": 200,
+                "body": results,
+            }
+        if method == "POST":
+            payload = request["requestContext"]["body"]
+            results = put_user(json.loads(payload))
+            return {
+                "statusCode": 201,
+                "body": results,
+            }
+    else:
+        return {
+            "statusCode": 404,
+            "body": "Not found",
+        }
+
+    with foo_db.cursor() as cursor:
+        cursor.execute(query)
+        for l in cursor:
+            print(l)
     brev.db_query("foo", "SHOW TABLES;")
     brev.db_query("foo", "SELECT COUNT(1);")
     now = datetime.now()
@@ -12,3 +47,70 @@ def handler(request):
         "request": request,
     }
 
+
+def init_db():
+    users_table = (
+        "CREATE TABLE IF NOT EXISTS `users` ("
+        "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+        "  `first_name` varchar(14) NOT NULL,"
+        "  `last_name` varchar(14) NOT NULL,"
+        "  PRIMARY KEY (`id`)"
+        ") ENGINE=InnoDB"
+    )
+    with foo_db.cursor() as c:
+        c.execute(users_table)
+
+def put_user(first_name, last_name):
+    statement = (
+        "INSERT INTO users "
+        "(first_name, last_name) "
+        "VALUES (%(first_name)s, %(last_name)s)"
+    )
+    data = {
+        "first_name": first_name,
+        "last_name": last_name,
+    }
+
+    result_id = 0
+    with foo_db.cursor() as c:
+        c.execute(statement, data)
+        result_id = c.lastrowid
+    foo_db.commit()
+
+    return {
+        "id": result_id,
+        "first_name": first_name,
+        "last_name": last_name,
+    }
+
+def get_users(user_id):
+    statement = (
+        "SELECT first_name, last_name FROM users "
+        "WHERE id = %(id)s"
+    )
+    data = {
+        "id": user_id,
+    }
+
+    results = {}
+    with foo_db.cursor() as c:
+        c.execute(statement, data)
+        for (user_id, first_name, last_name) in c:
+            results[user_id] = {
+                "id": user_id,
+                "first_name": first_name,
+                "last_name": last_name,
+            }
+    return results
+
+def delete_user(user_id):
+    statement = (
+        "DELETE FROM users "
+        "WHERE id = %(id)s"
+    )
+    data = {
+        "id": user_id,
+    }
+    with foo_db.cursor() as c:
+        c.execute(statement, data)
+    foo_db.commit()
