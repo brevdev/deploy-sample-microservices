8782fd3ea3717d99bcea2f2a113a2a451efc6c44
Drew Malin <drewmalin@gmail.com>
2021-04-16 16:23:57 -0700

    logging

--- source.js
+++ source.js
@@ -1,9 +1,13 @@
 const brev = require('brev');
 
 module.exports.handler = async (event, context) => {
+    console.log("in:")
+    console.log(event);
     return brev.service("targetjs")
         .get("/foo", {})
         .then((response) => {
+            console.log("out:")
+            console.log(event);
             return response;
         });
 };
--- target.js
+++ target.js
@@ -1,4 +1,5 @@
 module.exports.handler = async (event, context) => {
+    console.log(event);
     return {
         statusCode: 200,
         body: event,
