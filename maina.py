0bb0d8b52cf2e82d8b6daddd184fb47f305adeaa
Drew Malin <drewmalin@gmail.com>
2021-04-18 16:30:35 -0700

    json response

--- maina.py
+++ maina.py
@@ -3,5 +3,8 @@ from datetime import datetime
 def handler(request, context):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    return "main.a - " + current_time
+    return {
+        "statusCode": 200,
+        "body": main.a - " + current_time
+    }
 
